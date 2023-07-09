eee35d75df1dcc7d008da1c1510ea7ff0593926e
Danil <kabirdanilov14@gmail.com>
2023-07-10 00:44:25 +0500

    small fix

--- shared/ui/roundedButton.js
+++ shared/ui/roundedButton.js
@@ -6,13 +6,17 @@ export default function RoundedButton(props) {
   const isTheme = useThemeStore((state) => state.theme)
   return (
     <Pressable onPress={props.onPress || null}>
-      {({ isPressed }) => {
+      {({ pressed }) => {
         return (
           <View
             style={{
-              backgroundColor: isPressed
-                ? SwitchTheme(isTheme).bgbuttondefaultpressed
-                : SwitchTheme(isTheme).bgbuttondefault,
+              backgroundColor: pressed
+                ? isTheme.includes('theme_usual')
+                  ? SwitchTheme(isTheme).hoverBlue
+                  : SwitchTheme(isTheme).hoverEffect
+                : isTheme.includes('theme_usual')
+                ? SwitchTheme(isTheme).bgbuttondefault
+                : SwitchTheme(isTheme).checkIcon,
               paddingHorizontal: 90,
               paddingVertical: 10,
               width: '100%',
